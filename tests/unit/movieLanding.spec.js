d9f65cf0151497f1817ebb8bd525378aefe2d58c
Carlos Castro <carliianoide@hotmail.com>
2022-05-24 01:11:09 -0500

    feat: jest fully functional

---
+++ tests/unit/movieLanding.spec.js
@@ -0,0 +1,16 @@
+// import { shallowMount } from "@vue/test-utils";
+import Landing from "@/views/MovieCatalog/Landing/index.vue";
+
+describe("Landing", () => {
+  test("If lanfing page loaded, must show categories", () => {
+    expect(true).toBe(true);
+  });
+
+  test("If landing page loaded, must show popular movies", () => {
+    expect(true).toBe(true);
+  });
+
+  test("If landing page loaded, must show top rated movies", () => {
+    expect(true).toBe(true);
+  });
+});
